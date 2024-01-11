fb778caed0ada60dd2f14eaf9854562d98ee263c
FredLi <aikwdc00@hotmail.com>
2024-01-11 18:45:33 -0500

    12

--- libs-02-lodash-and-types/src/app.ts
+++ libs-02-lodash-and-types/src/app.ts
@@ -1,3 +1,14 @@
+/*
+lodash 是一个 JavaScript 工具库，提供了许多有用的工具函数，帮助开发者更方便地处理数组、对象、函数等数据类型。这些函数包括（但不限于）：
+
+数组操作（如 _.map、_.filter、_.reduce、_.find、_.every、_.some 等）
+对象操作（如 _.get、_.set、_.has、_.merge 等）
+函数操作（如 _.bind、_.curry、_.debounce、_.throttle 等）
+集合操作（如 _.groupBy、_.sortBy 等）
+数字、字符串、日期等操作
+其他实用函数（如 _.cloneDeep、_.isEqual、_.isEmpty 等）
+lodash 的设计目标是提供一套完整、一致的工具库，让 JavaScript 在各种使用场景下都能方便地使用。
+*/ 
 import _ from 'lodash';
 
 console.log(_.shuffle([1, 2, 3]));
