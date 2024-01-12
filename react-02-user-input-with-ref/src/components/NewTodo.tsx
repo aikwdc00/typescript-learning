c78cb51658b2056c78b40b6bdfe6fd2a63ee31cd
FredLi <aikwdc00@hotmail.com>
2024-01-12 13:55:05 -0500

    14 finish

--- react-02-user-input-with-ref/src/components/NewTodo.tsx
+++ react-02-user-input-with-ref/src/components/NewTodo.tsx
@@ -3,8 +3,11 @@ import React, { useRef } from 'react';
 const NewTodo: React.FC = () => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
+  // React.FormEvent的作用是阻止表单提交时浏览器的默认行为
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    
+    // !表示textInputRef.current一定不是null
     const enteredText = textInputRef.current!.value;
     console.log(enteredText);
   };
